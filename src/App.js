1d6a119c03aff3576e69e6fa15da8a5fe9a4a11b
amellalkamel <amellalkameleddine@gmail.com>
2021-01-12 11:56:01 +0100

    recupirer data from api

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import "./App.css";
-import Header from "./component/Header/Header";
-import Content from "./component/Contente/Content";
-import Main from "./component/Main/Main";
-import Profil from "./component/profil/Profil";
-import Evenement from "./component/Evenement/Evenement";
-
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Profil />
-      <Content />
-      <Main />
-      <Evenement />
-    </div>
-  );
+import './App.css';
+import React from 'react';
+import Header from './component/Header/Header';
+import Content from './component/Contente/Content';
+import Main from './component/Main/Main';
+import Profil from './component/profil/Profil';
+import Evenement from './component/Evenement/Evenement';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  async componentWillMount() {
+    const datas = await fetch('https://tt-sherpa-backend.herokuapp.com/events');
+    datas.json().then((data) => console.log('data is', data));
+  }
+  handleClick = () => {
+    console.log('je suis clicker');
+  };
+  render() {
+    return (
+      <div className="App">
+        <Header handleClick={this.handleClick} />
+        <Profil />
+        <Content />
+        <Main />
+        <Evenement />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,15 +1,16 @@
-import React from "react";
-import "./header.css";
+import React from 'react';
+import './header.css';
 class Header extends React.Component {
   constructor(props) {
     super(props);
+    console.log('props', this.props);
   }
 
   render() {
     return (
-      <div className="header">
+      <div className="header" onClick={this.props.handleClick}>
         <div className="title">
-          {" "}
+          {' '}
           <h1>Marketplace</h1>
         </div>
       </div>
